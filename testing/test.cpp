016c48c06aa0293c8707527559187152ab15f715
Comsty <vnkolobukhin@edu.hse.ru>
2025-02-27 14:57:40 +0300

    PlusMinus full realised and tested

--- testing/test.cpp
+++ testing/test.cpp
@@ -17,8 +17,8 @@ namespace
 		for (const auto &test : Tests)
 			MaxLen_TestName = std::max(MaxLen_TestName, test.TestName.length());
 
-		std::cout << "\n"
-				  << std::left << std::setw((int)MaxLen_TestName) << "Test Name" << "  Verdict";
+		std::cout
+			<< std::left << std::setw((int)MaxLen_TestName) << "Test Name" << "  Verdict";
 		std::cout << std::setfill('-') << std::setw((int)MaxLen_TestName + 10) << "\n"
 				  << std::setfill(' ');
 		std::cout << std::endl;
@@ -113,6 +113,90 @@ namespace
 		LongNumber expected = LongNumber("16561989191.010994", 35, 1000);
 		return ((expected - res) < 0.001_longnum);
 	}
+
+	bool testThatMinusWorks_IntMinimal()
+	{
+		// given
+		LongNumber a = 581_longnum;
+		LongNumber b = 692_longnum;
+
+		// when
+		LongNumber res = b - a;
+
+		// then
+		LongNumber expected = 111_longnum;
+		return (expected == res);
+	}
+
+	bool testThatMinusWorks_BiggerInt()
+	{
+		// given
+		LongNumber a = LongNumber("581929929919929299291", 100, 100);
+		LongNumber b = LongNumber("6921992929299291", 100, 100);
+
+		// when
+		LongNumber res = a - b;
+
+		// then
+		LongNumber expected = LongNumber("581923007927000000000", 100, 10);
+		return (expected == res);
+	}
+
+	bool testThatMinusWorks_FractionalBase2N()
+	{
+		// given
+		LongNumber a = LongNumber("581929929919929299218282882881991.0009765625", 110, 100);
+		LongNumber b = LongNumber("218282882881992", 100, 100);
+
+		// when
+		LongNumber res = a - b;
+
+		// then
+		LongNumber expected = LongNumber("581929929919929298999999999999999.0009765625", 110, 10);
+		return (expected == res);
+	}
+
+	bool testThatMinusWorks_FractionalBaseNot2N()
+	{
+		// given
+		LongNumber a = 0.1234_longnum;
+		LongNumber b = 1.1234_longnum;
+
+		// when
+		LongNumber res = a - b;
+
+		// then
+		LongNumber expected = -1_longnum;
+		return ((expected - res) < 0.00001_longnum);
+	}
+
+	bool testThatMinusWorks_NegativeInt()
+	{
+		// given
+		LongNumber a = -16277272_longnum;
+		LongNumber b = -188282_longnum;
+
+		// when
+		LongNumber res = a - b;
+
+		// then
+		LongNumber expected = -16088990_longnum;
+		return (expected == res);
+	}
+
+	bool testThatMinusWorks_NegativeFractional()
+	{
+		// given
+		LongNumber a = -16277272.123_longnum;
+		LongNumber b = 188282.1577_longnum;
+
+		// when
+		LongNumber res = a - b;
+
+		// then
+		LongNumber expected = -16465554.2807_longnum;
+		return ((expected - res) < 0.00001_longnum);
+	}
 }
 
 int main()
@@ -121,60 +205,112 @@ int main()
 	//
 	if (testThatPlusWorks_IntMinimal())
 	{
-		MyTests.push_back({"IntMinimal", true});
+		MyTests.push_back({"testThatPlusWorks_IntMinimal", true});
 	}
 	else
 	{
-		MyTests.push_back({"IntMinimal", false});
+		MyTests.push_back({"testThatPlusWorks_IntMinimal", false});
 	}
 	//
 	if (testThatPlusWorks_BiggerInt())
 	{
-		MyTests.push_back({"BiggerInt", true});
+		MyTests.push_back({"testThatPlusWorks_BiggerInt", true});
 	}
 	else
 	{
-		MyTests.push_back({"BiggerInt", false});
+		MyTests.push_back({"testThatPlusWorks_BiggerInt", false});
 	}
 	//
 	if (testThatPlusWorks_FractionalBase2N())
 	{
-		MyTests.push_back({"FractionalBase2N", true});
+		MyTests.push_back({"testThatPlusWorks_FractionalBase2N", true});
 	}
 	else
 	{
-		MyTests.push_back({"FractionalBase2N", false});
+		MyTests.push_back({"testThatPlusWorks_FractionalBase2N", false});
 	}
 	//
 	if (testThatPlusWorks_FractionalBaseNot2N())
 	{
-		MyTests.push_back({"FractionalBaseNot2N", true});
+		MyTests.push_back({"testThatPlusWorks_FractionalBaseNot2N", true});
 	}
 	else
 	{
-		MyTests.push_back({"FractionalBaseNot2N", false});
+		MyTests.push_back({"testThatPlusWorks_FractionalBaseNot2N", false});
 	}
 	//
 	if (testThatPlusWorks_NegativeInt())
 	{
-		MyTests.push_back({"NegativeInt", true});
+		MyTests.push_back({"testThatPlusWorks_NegativeInt", true});
 	}
 	else
 	{
-		MyTests.push_back({"NegativeInt", false});
+		MyTests.push_back({"testThatPlusWorks_NegativeInt", false});
 	}
 	//
 	if (testThatPlusWorks_NegativeFractional())
 	{
-		MyTests.push_back({"NegativeFractional", true});
+		MyTests.push_back({"testThatPlusWorks_NegativeFractional", true});
 	}
 	else
 	{
-		MyTests.push_back({"NegativeFractional", false});
+		MyTests.push_back({"testThatPlusWorks_NegativeFractional", false});
 	}
 	//
-
-
+	if (testThatMinusWorks_IntMinimal())
+	{
+		MyTests.push_back({"testThatMinusWorks_IntMinimal", true});
+	}
+	else
+	{
+		MyTests.push_back({"testThatMinusWorks_IntMinimal", false});
+	}
+	//
+	if (testThatMinusWorks_BiggerInt())
+	{
+		MyTests.push_back({"testThatMinusWorks_BiggerInt", true});
+	}
+	else
+	{
+		MyTests.push_back({"testThatMinusWorks_BiggerInt", false});
+	}
+	//
+	if (testThatMinusWorks_FractionalBase2N())
+	{
+		MyTests.push_back({"testThatMinusWorks_FractionalBase2N", true});
+	}
+	else
+	{
+		MyTests.push_back({"testThatMinusWorks_FractionalBase2N", false});
+	}
+	//
+	if (testThatMinusWorks_FractionalBaseNot2N())
+	{
+		MyTests.push_back({"testThatMinusWorks_FractionalBaseNot2N", true});
+	}
+	else
+	{
+		MyTests.push_back({"testThatMinusWorks_FractionalBaseNot2N", false});
+	}
+	//
+	if (testThatMinusWorks_NegativeInt())
+	{
+		MyTests.push_back({"testThatMinusWorks_NegativeInt", true});
+	}
+	else
+	{
+		MyTests.push_back({"testThatMinusWorks_NegativeInt", false});
+	}
+	//
+	if (testThatMinusWorks_NegativeFractional())
+	{
+		MyTests.push_back({"testThatMinusWorks_NegativeFractional", true});
+	}
+	else
+	{
+		MyTests.push_back({"testThatMinusWorks_NegativeFractional", false});
+	}
+	//
 	
 	PrintResults(MyTests);
 	return 0;
